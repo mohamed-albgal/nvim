e7fe43291aad93ee2a57e211a74400da10521d66
Mohamed Albgal <malbgal@gmail.com>
2023-08-06 15:21:10 -0700

    add floatterm

--- lua/custom/setup_config.lua
+++ lua/custom/setup_config.lua
@@ -4,3 +4,7 @@ api.nvim_command("autocmd TermOpen * startinsert")             -- starts in inse
 api.nvim_command("autocmd TermOpen * setlocal nonumber")       -- no numbers
 api.nvim_command("autocmd TermEnter * setlocal signcolumn=no") -- no sign column
 vim.keymap.set('t', '<esc>', "<C-\\><C-n>")                    -- esc to exit insert mode
+--
+-- Define the keymapping to copy file name and cursor's line number to register "+"
+vim.api.nvim_set_keymap('n', '<Leader>ry', ":let @+ = 'rspec ' . expand('%') . ':' . line('.')<CR>", { noremap = true, silent = true, desc = "copy rspec line signature" })
+vim.api.nvim_set_keymap('n', '<Leader>rf', ":let @+ = 'rspec ' . expand('%')<CR>", { noremap = true, silent = true , desc = "copy rspec file signature" })
