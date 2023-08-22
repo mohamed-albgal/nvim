89c1fe22658cc99fbc42389a53fd947afc58c9a5
Mohamed Albgal <malbgal@gmail.com>
2023-08-22 14:42:10 -0700

    open which key in v mode

--- init.lua
+++ init.lua
@@ -62,7 +62,7 @@ if vim.fn.exists("g:vscode") ~= 0 then
   vim.o.ignorecase = true
   vim.o.smartcase = true
   vim.o.clipboard = 'unnamedplus'
-  vim.keymap.set('n', '<leader>', "<cmd>call VSCodeNotify('whichkey.show')<CR>")
+  vim.keymap.set({'n','v'}, '<leader>', "<cmd>call VSCodeNotify('whichkey.show')<CR>")
   vim.keymap.set('n', 'gD', "<cmd>call VSCodeNotify('editor.action.revealDefinitionAside')<CR>")
   require('lazy').setup({
     {'ggandor/lightspeed.nvim'},
