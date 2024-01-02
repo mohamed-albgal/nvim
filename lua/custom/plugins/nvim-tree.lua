097f5f7761b23924a26c2ce9446e785db4f53262
Mohamed Albgal <malbgal@gmail.com>
2024-01-02 00:09:44 -0800

    tree commands

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -5,7 +5,11 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   }, 
-  keys = { { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Nvim Tree" } },
+  keys = { { "<leader>ee", "<cmd>NvimTreeToggle<cr>", desc = "Nvim Tree" },
+           { "<leader>es", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Nvim Tree find in tree" },
+           { "<leader>ey", "<cmd>NvimTreeClipboard<cr>", desc = "Nvim Tree Clipboard" }
+        },
+
   config = function()
     require("nvim-tree").setup {}
   end,
