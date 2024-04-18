3aa8a6645040e9fee94af13a255ec7290327b1b2
Mohamed Albgal <malbgal@gmail.com>
2024-04-17 19:37:36 -0700

    fix story creation command

--- lua/custom/keys.lua
+++ lua/custom/keys.lua
@@ -52,7 +52,32 @@ end
 function Story()
   local num = vim.fn.input("Enter number: ")
   local title = vim.fn.input("Enter the story title: ")
-  local branch_name = vim.fn.input("Enter the branch name: ")
+  -- local branch_name = vim.fn.input("Enter the branch name: ")
+
+  -- early return if num or title is empty
+  if num == "" or title == "" then
+    -- print("num, title, or branch name is empty")
+    print("num and title can't be empty")
+    return
+  end
+
+  -- if branch name is empty, use the title as the branch name, but replace spaces with dashes and skip any non-alphanumeric characters or numbers
+  -- if branch_name == "" then
+    -- change all numbers to empty string
+    branch_name = string.gsub(title, "%d", "")
+    -- change all non-alphanumeric characters to empty string
+    branch_name = string.gsub(branch_name, "%W", "")
+    -- change all spaces to dashes
+    branch_name = string.gsub(title, "%s+", "-")
+    -- remove any trailing dashes
+    branch_name = string.gsub(branch_name, "-$", "")
+    -- remove any leading dashes
+    branch_name = string.gsub(branch_name, "^-", "")
+    -- remove any dash that is followed by another dash
+    branch_name = string.gsub(branch_name, "-+", "-")
+    -- make it lowercase
+    branch_name = string.lower(branch_name)
+  -- end
 
   local formatted_output = string.format("%s - %s\n[Story](https://www.pivotaltracker.com/story/show/%s)\nwip/%s-%s", num, title, num, branch_name, num)
 
@@ -63,7 +88,7 @@ function Story()
   -- a newline before and after the formatted_output
   journal_file:write("\n" .. formatted_output .. "\n")
   journal_file:close()
-  print("Story information placed in register 3 and in today's journal file.")
+  print("-----------------------Story information placed in register 3 and in today's journal file.")
 end
 
 vim.cmd('command! Story lua Story()')
