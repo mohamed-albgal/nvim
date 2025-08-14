c845c364af64ebe8833dfb24ca8dad002d96abb3
Mohamed Albgal <mohamedalbgal@Mohameds-MacBook-Pro-2.local>
2025-08-14 14:50:32 -0700

    get vscode right

--- init.lua
+++ init.lua
@@ -2,9 +2,14 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require('bootstrap_lazy')
-require('lazy').setup({ { import = 'custom.plugins' } })
-require('custom.setup.core_setup')
-require('custom.setup.tree_sitter_setup')
-require('custom.keys')
-require("custom.commands")
-require("custom.setup.vscode_theme_setup")
+if vim.g.vscode then
+  require('lazy').setup({ spec = { { import = 'custom.plugins.flash' } } })
+  require('custom.setup.vscode_setup')
+else
+  require('lazy').setup({ spec = { { import = 'custom.plugins' } } })
+  require('custom.setup.core_setup')
+  require('custom.setup.tree_sitter_setup')
+  require('custom.keys')
+  require('custom.commands')
+  require('custom.setup.vscode_theme_setup')
+end
